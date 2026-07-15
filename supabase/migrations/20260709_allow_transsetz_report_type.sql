3a5a0cbf2e5c390fb47a279a981cabaf2aaf6c49
balajikalloji9 <bkalloji24@gmail.com>
2026-07-14 23:52:29 -0500

    Guard transsetz report migration

--- supabase/migrations/20260709_allow_transsetz_report_type.sql
+++ supabase/migrations/20260709_allow_transsetz_report_type.sql
@@ -1,23 +1,28 @@
 -- Allow transSet XML imports to be tracked in POS report files.
 
-alter table public.pos_report_files
-drop constraint if exists pos_report_files_report_type_check;
+do $$
+begin
+  if to_regclass('public.pos_report_files') is not null then
+    alter table public.pos_report_files
+    drop constraint if exists pos_report_files_report_type_check;
 
-alter table public.pos_report_files
-add constraint pos_report_files_report_type_check
-check (
-  report_type in (
-    'plu_sales',
-    'department_sales',
-    'category_sales',
-    'tax_summary',
-    'payment_summary',
-    'fuel_dcr_summary',
-    'deal_sales',
-    'cashier_summary',
-    'transsetz',
-    'unknown'
-  )
-);
+    alter table public.pos_report_files
+    add constraint pos_report_files_report_type_check
+    check (
+      report_type in (
+        'plu_sales',
+        'department_sales',
+        'category_sales',
+        'tax_summary',
+        'payment_summary',
+        'fuel_dcr_summary',
+        'deal_sales',
+        'cashier_summary',
+        'transsetz',
+        'unknown'
+      )
+    );
+  end if;
+end $$;
 
 notify pgrst, 'reload schema';
